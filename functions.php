614d7abe45baa52fcda6c83dd1d54ca2e91647f1
Ashley Gibson <ashleyg2431@tywls.org>
2016-07-29 13:48:41 -0400

    nav font color change

--- functions.php
+++ functions.php
@@ -14,3 +14,34 @@ add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
 
 
 
+<header>
+  <div class= "navbar navbar-default">
+    <div class="container">
+     <div class="navbar-header"
+       <button type= "button"class="navbar-toggle" data-toggle="collapse" data-target="#example">
+       	  <span class= "icon bar"></span>
+       	  <span class= "icon bar"></span>
+       	  <span class= "icon bar"></span>
+       </button>
+       <a href="" class="navbar-brand">Wiredwiki</a>
+  </div>
+  <div class="collapse navbar-collapse" id="example">
+  	 <ul class="nav navbar-nav">
+  	 	<li><a href="">Main</a></li>
+  	 	<li class= "active"><a href="">About</a></li>
+  	 	<li class= "active"><a href="">Portfolio</a></li>
+  	 	<li class= "active"><a href="">Contact</a></li>
+  	 	<li class= "active"><a href="">More</a></li>
+  	 </ul>
+
+
+
+
+
+
+
+
+
+
+
+
